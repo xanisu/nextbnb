3b17e528baf1433eaa89c7db261c5f9c5e3fa70d
Jordi Flores <jordi.flores@mecalux.com>
2021-12-07 10:11:13 +0100

    5/20 fs nextbnb start datepicker

--- pages/houses/[id].js
+++ pages/houses/[id].js
@@ -6,15 +6,32 @@ import Layout from '../../components/Layout'
 export default function House(props) {
   return (
     <Layout content={
-          <div>
+          <div className="container">
               <Head>
                   <title>{props.house.title}</title>
               </Head>
-              <img src={props.house.picture} width="100%" alt="House picture" />
-              <p>
-              {props.house.type} - {props.house.town}
-              </p>
-              <p>{props.house.title}</p>
+              <article>
+                <img src={props.house.picture} width="100%" alt="House picture" />
+                <p>
+                  {props.house.type} - {props.house.town}
+                </p>
+                <p>{props.house.title}</p>
+              </article>
+              <aside></aside>
+              <style jsx>{
+                `
+                  .container {
+                    display: grid;
+                    grid-template-columns: 60% 40%;
+                    grid-gap: 30px;
+                  }
+                  aside {
+                    border: 1px solid #ccc;
+                    padding: 20px;
+                  }
+                ` 
+              }
+              </style>
         </div>
       } />
   )
