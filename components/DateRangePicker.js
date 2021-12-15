3000d9ef730f44ec26bef0495d18f2cd712c4631
Jordi Flores <jordi.flores@mecalux.com>
2021-12-15 16:40:09 +0100

    11/20 fs airbnb show the price for the chosen dates

--- components/DateRangePicker.js
+++ components/DateRangePicker.js
@@ -34,7 +34,7 @@ const numberOfNightsBetweenDates = (startDate, endDate) => {
   return dayCount
 }
 
-export default function DateRangePicker() {
+export default function DateRangePicker({ datesChanged }) {
   const [startDate, setStartDate] = useState(today)
   const [endDate, setEndDate] = useState(tomorrow)
 
@@ -57,11 +57,12 @@ export default function DateRangePicker() {
           }}
           onDayChange={day => {
             setStartDate(day)
+            const newEndDate = new Date(day)
             if (numberOfNightsBetweenDates(day, endDate) < 1) {
-              const newEndDate = new Date(day)
               newEndDate.setDate(newEndDate.getDate() + 1)
               setEndDate(newEndDate)
             }
+            datesChanged(day, newEndDate)
           }}
         />
       </div>
@@ -85,6 +86,7 @@ export default function DateRangePicker() {
           }}
           onDayChange={day => {
             setEndDate(day)
+            datesChanged(startDate, day)
           }}
         />
       </div>
